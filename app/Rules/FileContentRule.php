faaf7365ecfefd23e51932f61f15cae3e29ad7cd
JosueOb <josuericardocando@hotmail.com>
2021-07-03 18:31:48 -0500

    Se valida la abreviación según los días registrados

--- app/Rules/FileContentRule.php
+++ app/Rules/FileContentRule.php
@@ -5,12 +5,15 @@ namespace App\Rules;
 
 class FileContentRule extends Rule
 {
-    protected string $pattern = "/^[a-zA-Z]{3,25}\=(\,?(MO|TU|WE|TH|FR|SA|SU)((0[0-9]|1[0-9]|2[0-3]):[0-5][0-9])\-((0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]))+$/";
+    //protected string $pattern = "/^[a-zA-Z]{3,25}\=(\,?(MO|TU|WE|TH|FR|SA|SU)((0[0-9]|1[0-9]|2[0-3]):[0-5][0-9])\-((0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]))+$/";
     public string $content;
+    protected string $pattern;
 
-    public function __construct(string $content)
+    public function __construct(string $content, array $days)
     {
-        $this->content = $content;
+        $pattern_days = implode( "|", $days);
+        $this->pattern = "/^[a-zA-Z]{3,25}\=(\,?($pattern_days)((0[0-9]|1[0-9]|2[0-3]):[0-5][0-9])\-((0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]))+$/";
+        $this->content = trim($content);
         parent::__construct($this->pattern, $this->content);
     }
 }
