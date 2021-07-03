bbeab422eb5703737aa8aec216499b7982a5f8bd
JosueOb <josuericardocando@hotmail.com>
2021-07-03 17:30:04 -0500

    Uso de helper para obtener una fecha bajo un formato

--- tests/Unit/PaymentTest.php
+++ tests/Unit/PaymentTest.php
@@ -12,8 +12,8 @@ class PaymentTest extends TestCase
 
     public function testHourlyPayment()
     {
-        $start = DateTime::createFromFormat("!H:i", "08:15");
-        $end = DateTime::createFromFormat("!H:i", "12:45");
+        $start = createDate("08:15");
+        $end = createDate("12:45");
         $hourly_price = 20;
 
         $payment = payForHoursWorked($start, $end, $hourly_price);
