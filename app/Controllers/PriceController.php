7c2e142cdcae50324fec17835293112c405fc845
JosueOb <josuericardocando@hotmail.com>
2021-07-03 09:10:07 -0500

    Configuración del controlador PriceController para la creación y búsqueda de precio

--- app/Controllers/PriceController.php
+++ app/Controllers/PriceController.php
@@ -12,13 +12,14 @@ class PriceController
 {
 
     /**
+     * @param array $prices
      * @param Type $type
      * @param Period $period
      * @param string $value
      * @return Price
      * @throws Exception
      */
-    static function create(Type $type, Period $period, string $value): Price
+    static function create(array $prices, Type $type, Period $period, string $value): Price
     {
         $number = new NumberRule($value);
 
@@ -26,6 +27,36 @@ class PriceController
             throw new Exception("The price $number->content is invalid.", 1);
         }
 
+        $search_prices = self::searchPriceByType($prices, $type->slug);
+        if (!empty($search_prices)) {
+            foreach ($search_prices as $price) {
+                if (
+                    $price->period->start === $period->start
+                    && $price->period->end === $period->end
+                ) {
+                    throw new Exception("The price $type->name {$period->getPeriod()["start"]} - {$period->getPeriod()["end"]} $ $number->content is already recorded.", 1);
+                }
+            }
+        }
+
         return new Price($type, $period, (float) $value);
+    }
+
+    /**
+     * @param array $prices
+     * @param $search
+     * @return array
+     */
+    static function searchPriceByType(array $prices, $search): array
+    {
+        //esto se realiza para retornar la key del arreglo a 0 ya que sino se entregaba la key del arreglo original
+        return array_values(
+            array_filter(
+                $prices,
+                function (Price $price) use ($search) {
+                    return $price->type->slug === $search;
+                }
+            )
+        );
     }
 }
