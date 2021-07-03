b5a3462d45de52a97c6442c547c1d85529b78c6a
JosueOb <josuericardocando@hotmail.com>
2021-07-03 12:50:42 -0500

    Prueba para el pago por periodo trabajado en base a registro de precios por periodo

--- tests/Unit/PaymentTest.php
+++ tests/Unit/PaymentTest.php
@@ -17,6 +17,30 @@ class PaymentTest extends TestCase
         $payment = payForHoursWorked($start, $end, $hourly_price);
 
         $this->assertIsFloat($payment);
-        
+    }
+
+    public function testPaymentByPeriod()
+    {
+        //Types
+        $working_day = createType("Working Day");
+
+        //Periods
+        $morning = createPeriod("00:01", "09:00");
+        $afternoon = createPeriod("09:01", "18:00");
+        $night = createPeriod("18:01", "00:00");
+
+        //Prices
+        $prices = [];
+        $wm_price = createPrice($prices, $working_day, $morning, "25");
+        array_push($prices, $wm_price);
+        $wa_price = createPrice($prices, $working_day, $afternoon, "15");
+        array_push($prices, $wa_price);
+        $wn_price = createPrice($prices, $working_day, $night, "20");
+        array_push($prices, $wn_price);
+
+        $period_worked = createPeriod("08:15", "18:45");
+        $payment = paymentByPeriod($prices, $period_worked);
+
+        $this->assertIsFloat($payment);
     }
 }
