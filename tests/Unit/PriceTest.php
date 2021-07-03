8cf8e20d99bb215aaaa6ba2bbdf2e745946e1fe9
JosueOb <josuericardocando@hotmail.com>
2021-07-03 09:05:07 -0500

    Prueba para la creación de precio y búsqueda en base a su tipo

--- tests/Unit/PriceTest.php
+++ tests/Unit/PriceTest.php
@@ -7,6 +7,7 @@ use App\Controllers\PriceController;
 use App\Controllers\TypeController;
 use App\Models\Price;
 use Exception;
+
 use PHPUnit\Framework\TestCase;
 
 class PriceTest extends TestCase
@@ -19,8 +20,39 @@ class PriceTest extends TestCase
         $type = TypeController::create("Working Day");
         $morning = PeriodController::create("09:00", "18:00");
 
-        $price = PriceController::create($type, $morning, "15");
+        $prices = [
+            //PriceController::create([], $type, $morning, "15")
+        ];
+
+        $price = PriceController::create($prices, $type, $morning, "15");
 
         $this->assertInstanceOf(Price::class, $price);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testSearchPriceByType()
+    {
+        //Types
+        $working_day = createType("Working Day");
+        $holiday = createType("Holiday");
+
+        //Periods
+        $morning = createPeriod("00:01", "09:00");
+        $afternoon = createPeriod("09:01", "18:00");
+        
+        //Prices
+        $prices = [];
+        $wm_price = createPrice($prices, $working_day, $morning, "25");
+        array_push($prices, $wm_price);
+        $ha_price = createPrice($prices, $holiday, $afternoon, "20");
+        array_push($prices, $ha_price);
+
+        $search_price1 = searchPriceByType($prices, $working_day->slug);
+        $search_price2 = searchPriceByType($prices, "hello");
+
+        $this->assertIsArray($search_price1);
+        $this->assertEmpty($search_price2);
     }
 }
