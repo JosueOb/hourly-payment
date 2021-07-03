d1a05011a37c5fe39d952ef3d89bc84704f14815
JosueOb <josuericardocando@hotmail.com>
2021-07-03 18:34:18 -0500

    Corrección de la prueba para la lectura del contenido del archivo

--- tests/Unit/FileTest.php
+++ tests/Unit/FileTest.php
@@ -13,7 +13,28 @@ class FileTest extends TestCase
      */
     public function testReadFile()
     {
-        $file_content = redFile('data.txt');
+        //Types
+        $working_day = createType("Working Day");
+        $holiday = createType("Holiday");
+        
+        //Days
+        $days = [];
+        $monday = createDay($days, "Monday", "MO", $working_day);
+        array_push($days, $monday);
+        $tuesday = createDay($days, "Tuesday", "TU", $working_day);
+        array_push($days, $tuesday);
+        $wednesday = createDay($days, "Wednesday", "WE", $working_day);
+        array_push($days, $wednesday);
+        $thursday = createDay($days, "Thursday", "TH", $working_day);
+        array_push($days, $thursday);
+        $friday = createDay($days, "Friday", "FR", $working_day);
+        array_push($days, $friday);
+        $saturday = createDay($days, "Saturday", "SA", $holiday);
+        array_push($days, $saturday);
+        $sunday = createDay($days, "Sunday", "SU", $holiday);
+        array_push($days, $sunday);
+
+        $file_content = redFile('data.txt', $days);
 
         $this->assertIsArray($file_content);
     }
